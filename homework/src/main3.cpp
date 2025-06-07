c8ed583c1e48645c336db1ee633fede37174ce80
sleepzzzewe <s2005921011@gmail.com>
2025-06-07 16:24:30 +0800

    chage_main3.cpp

--- homework/src/main3.cpp
+++ homework/src/main3.cpp
@@ -2,43 +2,32 @@
 #include <thread>
 #include <chrono>
 
-using namespace std;
-
-void io(char buf, int id)
-{
-    cout << "[I/O]  Buffer " << buf << " block " << id << " loaded\n";
-    this_thread::sleep_for(chrono::milliseconds(20));
+void io(char buf,int id){
+    std::cout << "[I/O]  Buffer " << buf << " block " << id << " loaded\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
 }
-void cpu(char buf, int id)
-{
-    cout << "[CPU]  Sort   Buffer " << buf << " block " << id << '\n';
-    this_thread::sleep_for(chrono::milliseconds(10));
+void cpu(char buf,int id){
+    std::cout << "[CPU]  Sort   Buffer " << buf << " block " << id << '\n';
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
-int main()
-{
-    constexpr int BLOCK  = 2000;
-    constexpr int TOTAL  = 200000;
-    constexpr int BLOCKS = TOTAL / BLOCK;   // 100
+int main(){
+    constexpr int BLOCKS = 100;   // 200k records / 2k records per block
 
-    for (int i = 0; i < BLOCKS; ++i)
-    {
-        if (i & 1)
-        {
-            thread t(io, 'B', i + 1);
-            cpu('A', i);
+    for(int i = 0; i < BLOCKS; ++i){
+        if(i & 1){
+            std::thread t(io,'B',i+1);
+            cpu('A',i);
             t.join();
-        }
-        else
-        {
-            thread t(io, 'A', i + 1);
-            cpu('B', i);
+        }else{
+            std::thread t(io,'A',i+1);
+            cpu('B',i);
             t.join();
         }
     }
-
-    cout << "\nMax input rate ≈ 100 000 records/s\n";
+    std::cout << "\nMax input rate ~100 000 records/s\n";
     return 0;
 }
+
 
 
